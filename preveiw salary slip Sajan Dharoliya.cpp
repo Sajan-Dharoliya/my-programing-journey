25c8b543f0ecdcab0b173fb580dc17a3d102a74f
Sajan Dharoliya <dharoliyasajan@gmail.com>
2026-04-17 21:08:42 +0530

    Add files via upload
    i think its not usefull for you, but this is means to mee :)

---
+++ preveiw salary slip Sajan Dharoliya.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int num; string name;
+	cout<<"enter your name"<<endl;
+	cin>>name;
+	if (name=="sajan"){
+		cout<<"enter your employee number "<<endl;
+		cin>>num;
+	}
+	
+	else {
+	cout<<"not corecct enter again "<<endl;
+	}
+	
+	if (num==44){
+		cout<<"date: // month // year 31st "<<endl<<"31st salary slip:"<<endl<<"employee name: Sajan Dharoliya, employee no44. "<<
+		endl<<"Department no.2"<<endl<<"Work Role: Senior AI engineer"<<endl<<"gross salary 100000Rs,"<<endl<<"Total TAX Deduction,"<<endl<<"professional tax deduction Rs200,"<<endl
+		<<"provident fund deduction Rs1800, "<<endl
+		<<"income tax deduction Rs3500, "<<endl<<"total net salary employee no.44 Rs94500, "<<endl;
+	}
+	else {
+		cout<<"invalid employee input!"<<endl;
+	}
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+}

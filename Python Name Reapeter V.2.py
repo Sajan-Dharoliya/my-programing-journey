f5c35b8838192aa0a2be3d8c71c9853937f2e46d
Sajan Dharoliya <dharoliyasajan@gmail.com>
2026-04-23 20:42:40 +0530

    Simple Python Name Reapeter Block V2
    Python name Reapeter V.2

---
+++ Python Name Reapeter V.2.py
@@ -0,0 +1,12 @@
+name=input("Enter Your  Name: ")#  Enter Your Name:
+
+if name=="sajan":# I Choose My Name, You Choose Anything To Print:
+    
+    reap=int(input("Enter Number You Have To Reapet Your Name: "))#Enter Your Name OR Your Set Text:
+    
+    for reap in range(reap):# loop
+         print(name,"",reap)
+  
+  
+else:
+     print("Enter Correct Name: ")# Not Correct Text To satisfy Your Input:

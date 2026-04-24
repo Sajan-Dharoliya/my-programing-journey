d3daffe5b46c5e134a043504aa82481b4322e353
Sajan Dharoliya <dharoliyasajan@gmail.com>
2026-04-24 19:32:43 +0530

    Odd  Even and Name repeat block
    choose Mode For Personal Use

---
+++ Odd  Even and Name repeat block.py
@@ -0,0 +1,40 @@
+a=int(input("Enter Mode (1 for Even-Odd, 2 For Name Reapet: )"))
+
+if a==1:
+    ev=int(input("Enter Your Number To Find Odd Or Even :"))
+    if ev%2 == 0:
+        print(ev,"Is Even")
+
+    else:
+        print(ev,"Is Odd")
+
+elif a==2:
+    name=input("Enter Your Name: ")
+
+    reap=int(input("Enter Number You Have To Raepet Your Name: "))
+
+    for reap in range(reap):
+        print(name,reap)
+
+else:
+    print("Wrong Option!",a,"Choose 1 Or 2")
+    a=int(input("Enter Mode (1 for Even-Odd, 2 For Name Reapet: )"))
+
+if a==1:
+    ev=int(input("Enter Your Number To Find Odd Or Even :"))
+    if ev%2 == 0:
+        print(ev,"Is Even")
+
+    else:
+        print(ev,"Is Odd")
+
+elif a==2:
+    name=input("Enter Your Name: ")
+
+    reap=int(input("Enter Number You Have To Raepet Your Name: "))
+
+    for reap in range(reap):
+        print(name,reap)
+
+else:
+    print("Wrong Option!",a,"Choose 1 Or 2")

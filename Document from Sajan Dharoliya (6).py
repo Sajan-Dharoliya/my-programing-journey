0b3896a288ac22d6e9eb9026e94d07a88934c8f6
Sajan Dharoliya <dharoliyasajan@gmail.com>
2026-05-23 23:52:30 +0530

    Hii back and do some future thing
    Pandas future task

---
+++ Document from Sajan Dharoliya (6).py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+data={#              see this is a simple table
+    'name':['sajan'],
+    'salary':[100000],
+    'role':['Ai Engineer'],
+    'goal':['to become best Ai Engineer'],
+    'current state':['just learning...']
+
+}
+
+
+Future_df=pd.DataFrame(data)
+
+print(Future_df)
+
+print(type(Future_df)) # i update this in future
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
